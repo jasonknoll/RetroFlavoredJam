c0724e34acb1d94f83822d5d6c1a2d89edff2e5a
Jason Knoll <jasonknoll98@aol.com>
2024-11-01 17:28:04 -0400

    added player input to move puck

--- src/main.c
+++ src/main.c
@@ -2,6 +2,9 @@
 #include <stdint.h>
 #include "puck_sprite.h"
 
+#define SCREEN_WIDTH 160
+#define SCREEN_HEIGHT 144
+
 // TODO 
 // Add test tilemap background
 // Add test player sprite
@@ -11,21 +14,72 @@
 // Add net
 // Add CPU opponent 
 // Add CPU opponent goalie
+// Add program state
 
 // From the test_func.s file
 extern int8_t test_func();
 
+uint8_t puck_x = 8;
+uint8_t puck_y = 16;
+
+void check_input() {
+    if (joypad() & J_A) {
+
+    }
+
+    if (joypad() & J_START) {
+
+    }
+
+    if (joypad() & J_LEFT) {
+        if (puck_x >= 8) 
+            puck_x--;
+    }
+
+    else if (joypad() & J_RIGHT) {
+        if (puck_x <= 160)
+            puck_x++;
+    }
+    
+
+    if (joypad() & J_UP) {
+        if (puck_y >= 16)
+            puck_y--;
+    }
+
+    else if (joypad() & J_DOWN) {
+        if (puck_y <= 144)
+            puck_y++;
+    } 
+    
+
+    delay(8);
+}
+
 int main() {
+
     SPRITES_8x8;
 
     int8_t test = test_func();
 
+    // Setting test to 1 in the assembly
     if (test == 1) {
         set_sprite_data(0, 1, sprite);
         set_sprite_tile(0, 0);
-        move_sprite(0, 8, 16);
+        move_sprite(0, puck_x, puck_y);
         SHOW_SPRITES;
+    }
+
+    joypad();
+
+    for (;;) {
+        // TODO main gameplay loop
+        // TODO handle input
+
+        check_input();
+        move_sprite(0, puck_x, puck_y);
     }
 
     return 0;
 }
+
